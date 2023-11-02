defbb8f2d82b0a88f7eaaa2c614efbcd8f05c318
alexzhangmaker <64229826+alexzhangmaker@users.noreply.github.com>
2023-11-02 19:19:28 +0700

    daily checkin

--- index.js
+++ index.js
@@ -28,46 +28,14 @@ app.use(express.json());
 const urlSignpost = process.env.urlSignpost;//'http://127.0.0.1:8080/' ;
 const projectId = process.env.SignpostGoogleProjectId;//'athena-396606';
 
-
 const userID = 'alexszhang@gmail.com' ;
 const port = parseInt(process.env.FalconServerPort);
 
 
+//http://127.0.0.1:8080/json?mm=10
+app.get('/json', serveIncomeStream);
+app.get('/dryRun', _dryRun);
 
-
-//
-//  Client facing request routing
-// 
-app.get('/json', servePortal);
-
-//app.get('/json', userFetchCollections);
-//app.get('/article', userFetchArticle) ;
-//app.get('/notes/:userID/:documentID', userFetchArticleNotes) ;
-//app.get('/tags', userFetchTags) ;
-
-//app.post('/notes/upload',userUploadArticleNote) ;
-app.post('/', servePostPing);
-//app.post('/removeArtciles', userRemoveArticles) ;
-//app.get('/crawl',userRequestCrawl) ;
-//app.get('/readeren',userRequestRender) ;
-//app.get('/saveItLater', userRequestSaveItLater) ;
-
-//app.post('/admin/registerPlugin', adminRegisterPlugin) ;
-//app.post('/admin/UnregisterPlugin', adminUnregisterPlugin) ;
-
-//app.get('/admin/console', adminConsole) ;
-
-/*
-app.get('/fruit/:userID/:documentID', function(req, res) {
-  console.log(req.params.userID, req.params.documentID) ;
-  let jsonResp = {
-    "one":"good",
-    "two":"man"
-  };
-  res.json(jsonResp) ;
-
-});
-*/
 
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
@@ -75,19 +43,116 @@ app.listen(port, () => {
 
 
 
-async function servePortal(req, res){
-    console.log('servePortal') ;
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
-}
-
-async function servePostPing(req, res){
-  const name = process.env.NAME || 'World';
-  console.log(req.body) ;
-  let jsonResp = {
-    "name":"alexszhang",
-    "email":"alexszhang@gmail.com"
+async function serveIncomeStream(req, res){
+  let cMonth = req.query.mm ;
+  console.log('serveIncomeStream') ;
+  let jsonGlobalIncome={
+    "date":"2023/10/31",
+    "incomes":[
+        {
+            "date":"2023/10/01",
+            "income":200.00,
+            "currency":"THB",
+            "source":"Arise Condo",
+            "class":"Rental",
+            "status":"collected"
+          },{
+            "date":"2023/10/01",
+            "income":200.00,
+            "currency":"USD",
+            "source":"PGX",
+            "class":"Devidend",
+            "status":"scheduled"
+          }
+    ]
   } ;
-  res.json(jsonResp);
+
+  let jsonIncome = {
+    "date":"2023/10/31",
+    "incomes":await runQueryIncome(cMonth)
+  };
+  
+  console.log(JSON.stringify(jsonIncome,null,3)) ;
+
+  res.json(jsonIncome);
 }
 
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://alexszhang:ChinaNO0001.@athenacluster0.ls5vshu.mongodb.net/?retryWrites=true&w=majority";
+
+
+
+
+async function _dryRun(req, res){
+  res.send('dryRun request receieved') ;
+  console.log('_dryRun') ;
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+
+}
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function runQueryIncome(cMonth) {
+  let incomeStream={} ;
+  try {
+      await client.connect();
+      // Get the database and collection on which to run the operation
+      const database = client.db("falcon");
+      const incomeStatement = database.collection("incomeStatement");
+      // Query for movies that have a runtime less than 15 minutes
+      const query = { 
+          year: "2023",
+          month:cMonth 
+      };
+      /*
+      const options = {
+      // Sort returned documents in ascending order by title (A->Z)
+      sort: { title: 1 },
+      // Include only the `title` and `imdb` fields in each returned document
+      projection: { _id: 0, title: 1, imdb: 1 },
+      };
+      */
+      // Execute query 
+      const cursor = incomeStatement.find(query/*, options*/);
+      // Print a message if no documents were found
+      if ((await incomeStatement.countDocuments(query)) === 0) {
+          console.log("No documents found!");
+      }
+      // Print returned documents
+      for await (const incomeDoc of cursor) {
+          //console.dir(incomeDoc.incomeStream);
+          incomeStream = incomeDoc.incomeStream ;
+      }
+  } finally {
+      await client.close();
+      return incomeStream ;
+  }
+}
